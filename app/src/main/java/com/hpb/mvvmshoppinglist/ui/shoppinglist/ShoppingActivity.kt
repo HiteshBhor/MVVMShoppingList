ab4d48ee97fb3b6d86a925fa26500d8c1917af5d
Aadyam <aadyam.developer@gmail.com>
2022-02-16 16:20:26 +0530

    KODEIN DEPENDENCY INJECTION - Part 6

--- app/src/main/java/com/hpb/mvvmshoppinglist/ui/shoppinglist/ShoppingActivity.kt
+++ app/src/main/java/com/hpb/mvvmshoppinglist/ui/shoppinglist/ShoppingActivity.kt
@@ -12,15 +12,18 @@ import com.hpb.mvvmshoppinglist.data.db.entities.ShoppingItem
 import com.hpb.mvvmshoppinglist.data.repositories.ShoppingRepository
 import com.hpb.mvvmshoppinglist.other.ShoppingItemAdapter
 import kotlinx.android.synthetic.main.activity_shopping.*
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.kodein
+import org.kodein.di.generic.instance
 
-class ShoppingActivity : AppCompatActivity() {
+class ShoppingActivity : AppCompatActivity(), KodeinAware {
+
+    override val kodein by kodein()
+    private val factory: ShoppingViewModelFactory by instance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shopping)
-
-        val database = ShoppingDatabase(this)
-        val repository = ShoppingRepository(database)
-        val factory = ShoppingViewModelFactory(repository)
 
         val viewModel = ViewModelProviders.of(this, factory).get(ShoppingViewModel::class.java)
 
